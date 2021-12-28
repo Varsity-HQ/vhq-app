958ab95498aaac52e64457ec157900c3e1af34e4
Harmony <harmochiky2@gmail.com>
2021-12-29 01:55:08 +0200

    Optimized home for perfomance

---
+++ app/components/Home/Header.js
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  View,
+  Image as LocalImage,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Image } from "react-native-expo-image-cache";
+import universityShortName from "../../util/universityShortName";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import Text from "../AppText";
+import TabNavigator from "../TabNavigator";
+import colors from "../../config/colors";
+
+const home_tabs = [
+  {
+    title: "All",
+    index: 1,
+    icon: (
+      <Ionicons color={colors.white} size={18} name="ios-file-tray-stacked" />
+    ),
+  },
+  {
+    title: "Events",
+    index: 2,
+    icon: <FontAwesome color={colors.white} size={16} name="calendar-o" />,
+  },
+  {
+    title: "Discover",
+    index: 3,
+    icon: (
+      <FontAwesome color={colors.white} size={16} name="arrow-circle-o-right" />
+    ),
+  },
+  {
+    title: "Offers",
+    index: 4,
+    icon: <FontAwesome color={colors.white} size={16} name="tags" />,
+  },
+];
+
+const Header = (props) => {
+  const { navigation, profilepic, username, university, loading, posts } =
+    props;
+
+  const [index, setTab] = useState(1);
+
+  return (
+    <View>
+      <View style={styles.header}>
+        <View style={{ width: "30%" }}>
+          <TouchableWithoutFeedback
+            onPress={() =>
+              navigation.navigate("Profile", { username: username })
+            }
+          >
+            {profilepic ? (
+              <Image
+                transitionDuration={300}
+                style={styles.profilepic}
+                uri={profilepic}
+              />
+            ) : (
+              <LocalImage
+                style={styles.profilepic}
+                source={require("../../assets/avatar.png")}
+              />
+            )}
+          </TouchableWithoutFeedback>
+        </View>
+        <Text allowFontScaling={false} style={styles.vhq_title}>
+          VarsityHQ
+        </Text>
+        <View style={styles.header_uni_container}>
+          <View style={styles.header_uni_wrapper}>
+            <FontAwesome name="university" color={colors.secondary} size={15} />
+            <Text style={styles.header_uni_text}>
+              {universityShortName(university)}
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.tabbar_container}>
+        <TabNavigator
+          active={index}
+          onPress={(i) => setTab(i)}
+          items={home_tabs}
+        />
+      </View>
+      <View
+        style={{
+          // padding: 10,
+          borderBottomColor: "black",
+          position: "relative",
+          borderTopWidth: 2,
+          // marginTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            padding: 18,
+            fontSize: 18,
+            color: "#fff",
+            zIndex: 1,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Text style={{ fontWeight: "700" }}>Timeline | </Text>
+          <Text style={{ fontSize: 14 }}>showing recent posts</Text>
+        </Text>
+        <LinearGradient
+          colors={["#1160af", "#9e7b9b"]}
+          // colors={["red", "white"]}oin
+          style={styles.grad_diverder}
+          start={[0, 0]}
+          end={[1, 0]}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  grad_diverder: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    height: "100%",
+    width: "100%",
+  },
+
+  tab: {
+    marginLeft: 10,
+    height: "100%",
+  },
+  tabbar_container: {
+    borderTopWidth: 3,
+    borderRadius: 0,
+    borderTopColor: colors.primary,
+    backgroundColor: colors.dark,
+  },
+  profilepic: {
+    height: 50,
+    width: 50,
+    marginLeft: 15,
+    borderRadius: 100,
+    borderWidth: 3,
+    borderColor: colors.dark_opacity_2,
+  },
+  header_uni_text: {
+    marginLeft: 7,
+    color: colors.secondary,
+    fontWeight: "700",
+    alignSelf: "center",
+    fontSize: 16,
+  },
+  header_uni_wrapper: {
+    borderWidth: 2,
+    borderColor: colors.secondary,
+    borderRadius: 50,
+    borderRightWidth: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: "row",
+    alignItems: "center",
+
+    // height: 10,
+  },
+  header_uni_container: {
+    width: "30%",
+    display: "flex",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    // height: 10,
+  },
+  vhq_title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.white,
+    fontFamily: "Lobster-Regular",
+    width: "40%",
+  },
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+  },
+  container: {},
+});
+
+export default Header;
--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, PureComponent } from "react";
 import {
   View,
   StyleSheet,
@@ -10,32 +10,23 @@ import {
 } from "react-native";
 import colors from "../config/colors";
 import { FontAwesome, Ionicons, Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
-import SkeletonComponent from "./Skeletons/SkeletonComponent";
 import SkeletonPost from "./Skeletons/Post";
 import dayjs from "dayjs";
 import Localize from "dayjs/plugin/relativeTime";
 import Content from "./Post/content";
 import PostMenu from "./Post/PostMenu";
 import { Image } from "react-native-expo-image-cache";
-import ParsedText from "react-native-parsed-text";
 
 import * as routes from "../navigation/routes";
 
 dayjs.extend(Localize);
 
-function PostCard({ data }) {
-  useEffect(() => {
-    return;
-  }, []);
-
-  const navigation = useNavigation();
-
-  const profilepic = (uri) => {
+class PostCard extends PureComponent {
+  profilepic = (uri) => {
     let image_uri = "";
 
     if (uri) {
-      image_uri = data.profilepic;
+      image_uri = this.props.data.profilepic;
     } else {
       image_uri = require("../assets/avatar.png");
     }
@@ -43,147 +34,152 @@ function PostCard({ data }) {
     return image_uri;
   };
 
-  if (!data) return <SkeletonPost />;
+  render() {
+    const data = this.props.data;
 
-  return (
-    <>
-      <View style={styles.container}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <TouchableWithoutFeedback
-            onPress={() =>
-              navigation.push(routes.PROFILE, { username: data.username })
-            }
-          >
-            <View style={{ flexDirection: "row" }}>
-              {data.profilepic ? (
-                <Image
-                  style={styles.p_avatar}
-                  uri={profilepic(data.profilepic)}
-                  transitionDuration={300}
-                />
-              ) : (
-                <ImageLocal
-                  style={styles.p_avatar}
-                  uri={profilepic(data.profilepic)}
-                  source={require("../assets/avatar.png")}
-                />
-              )}
+    if (!data) return <SkeletonPost />;
 
-              <View style={{ marginLeft: 10 }}>
-                <View style={styles.post_header}>
-                  <Text
-                    ellipsizeMode="tail"
-                    numberOfLines={1}
-                    lineBreakMode="tail"
-                    style={styles.u_name}
-                  >
-                    {data.firstname}&nbsp;{data.surname.charAt(0)}
-                  </Text>
-                  <Text style={styles.date_posted}>
-                    {dayjs(data.created_at).fromNow()}
-                  </Text>
-                </View>
-                <Text style={styles.username}>
-                  @{data.username} - <FontAwesome name="university" size={12} />
-                </Text>
-              </View>
-            </View>
-          </TouchableWithoutFeedback>
-          <View style={{ marginRight: 10 }}>
-            <PostMenu />
-          </View>
-        </View>
-        <View style={styles.content_container}>
-          <TouchableWithoutFeedback onPress={() => console.log("clicked")}>
-            <Content html={data.postHtmlText} />
-          </TouchableWithoutFeedback>
-
-          {/* <Text
-          onPress={() => nav.navigate("PostPage")}
-          style={{ fontSize: 16, color: colors.light }}
-        >
-          {JSON.stringify(data.caption)}
-        </Text> */}
-        </View>
-        <View
-          style={{
-            // marginTop: 15,
-            justifyContent: "space-between",
-            flexDirection: "row",
-          }}
-        >
-          <Text style={{ fontSize: 14, color: colors.secondary }}>
-            {parseInt(data.likes_count) + parseInt(data.comments_count)}{" "}
-            interactions
-          </Text>
-          <View>
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <ImageLocal
-                style={{
-                  height: 18,
-                  width: 18,
-                  marginRight: 5,
-                }}
-                source={require("../assets/vhqcat-small.png")}
-              />
-              <Text style={{ fontSize: 13, color: "#4f708a" }}>
-                {/* {data.application} */}
-                VarsityHQ ~{""}
-                {Platform.OS === "ios" && " iPhone"}
-                {Platform.OS === "android" && " Android"}
-                {Platform.OS === "web" && " Web"}
-                {Platform.OS === "macos" && " Mac"}
-                {Platform.OS === "windows" && " Windows"}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            justifyContent: "space-between",
-            flexDirection: "row",
-            marginTop: 10,
-          }}
-        >
+    return (
+      <>
+        <View style={styles.container}>
           <View
             style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <TouchableWithoutFeedback
+              onPress={() =>
+                this.props.navigation.push(routes.PROFILE, {
+                  username: data.username,
+                })
+              }
+            >
+              <View style={{ flexDirection: "row" }}>
+                {data.profilepic ? (
+                  <Image
+                    style={styles.p_avatar}
+                    uri={this.profilepic(data.profilepic)}
+                    transitionDuration={300}
+                  />
+                ) : (
+                  <ImageLocal
+                    style={styles.p_avatar}
+                    uri={this.profilepic(data.profilepic)}
+                    source={require("../assets/avatar.png")}
+                  />
+                )}
+
+                <View style={{ marginLeft: 10 }}>
+                  <View style={styles.post_header}>
+                    <Text
+                      ellipsizeMode="tail"
+                      numberOfLines={1}
+                      lineBreakMode="tail"
+                      style={styles.u_name}
+                    >
+                      {data.firstname}&nbsp;{data.surname.charAt(0)}
+                    </Text>
+                    <Text style={styles.date_posted}>
+                      {dayjs(data.created_at).fromNow()}
+                    </Text>
+                  </View>
+                  <Text style={styles.username}>
+                    @{data.username} -{" "}
+                    <FontAwesome name="university" size={12} />
+                  </Text>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+            <View style={{ marginRight: 10 }}>
+              <PostMenu />
+            </View>
+          </View>
+          <View style={styles.content_container}>
+            <Content html={data.postHtmlText} />
+
+            {/* <Text
+            onPress={() => nav.navigate("PostPage")}
+            style={{ fontSize: 16, color: colors.light }}
+          >
+            {JSON.stringify(data.caption)}
+          </Text> */}
+          </View>
+          <View
+            style={{
+              // marginTop: 15,
               justifyContent: "space-between",
               flexDirection: "row",
             }}
           >
-            <View style={styles.button}>
-              <Ionicons name="heart" size={26} color={colors.white} />
-              <Text style={styles.button_text}>{data.likes_count}</Text>
-            </View>
-            <View style={styles.button}>
-              <Ionicons
-                name="ios-chatbubbles-outline"
-                size={25}
-                color={colors.white}
-              />
-              <Text style={styles.button_text}>{data.comments_count}</Text>
-            </View>
-            <View style={styles.button}>
-              <Ionicons
-                name="ios-chatbox-ellipses-outline"
-                size={26}
-                color={colors.white}
-              />
+            <Text style={{ fontSize: 14, color: colors.secondary }}>
+              {parseInt(data.likes_count) + parseInt(data.comments_count)}{" "}
+              interactions
+            </Text>
+            <View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <ImageLocal
+                  style={{
+                    height: 18,
+                    width: 18,
+                    marginRight: 5,
+                  }}
+                  source={require("../assets/vhqcat-small.png")}
+                />
+                <Text style={{ fontSize: 13, color: "#4f708a" }}>
+                  {/* {data.application} */}
+                  VarsityHQ ~{""}
+                  {Platform.OS === "ios" && " iPhone"}
+                  {Platform.OS === "android" && " Android"}
+                  {Platform.OS === "web" && " Web"}
+                  {Platform.OS === "macos" && " Mac"}
+                  {Platform.OS === "windows" && " Windows"}
+                </Text>
+              </View>
             </View>
           </View>
-          <View style={styles.button}>
-            <Feather name="bookmark" size={26} color={colors.white} />
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              marginTop: 10,
+            }}
+          >
+            <View
+              style={{
+                justifyContent: "space-between",
+                flexDirection: "row",
+              }}
+            >
+              <View style={styles.button}>
+                <Ionicons name="heart" size={26} color={colors.white} />
+                <Text style={styles.button_text}>{data.likes_count}</Text>
+              </View>
+              <View style={styles.button}>
+                <Ionicons
+                  name="ios-chatbubbles-outline"
+                  size={25}
+                  color={colors.white}
+                />
+                <Text style={styles.button_text}>{data.comments_count}</Text>
+              </View>
+              <View style={styles.button}>
+                <Ionicons
+                  name="ios-chatbox-ellipses-outline"
+                  size={26}
+                  color={colors.white}
+                />
+              </View>
+            </View>
+            <View style={styles.button}>
+              <Feather name="bookmark" size={26} color={colors.white} />
+            </View>
           </View>
         </View>
-      </View>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
