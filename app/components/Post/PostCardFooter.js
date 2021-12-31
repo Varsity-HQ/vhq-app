d563f49d47274e1394869d40e4ba0c4ba1449b75
Harmony <harmochiky2@gmail.com>
2021-12-31 22:13:12 +0200

    Progress to here

--- app/components/Post/PostCardFooter.js
+++ app/components/Post/PostCardFooter.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Animated, TouchableOpacity } from "react-native";
 import {
   View,
@@ -12,15 +12,104 @@ import colors from "../../config/colors";
 import Text from "../AppText";
 import { Ionicons, Feather } from "@expo/vector-icons";
 import * as Animatable from "react-native-animatable";
+import axios from "axios";
+import db from "../../util/fb_admin";
+const {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+} = require("firebase/firestore");
 
 const mapStateToProps = (state) => {
   return {
     profilepic: state.core.accData.profilepic,
+    username: state.core.accData.username,
   };
 };
 
-function PostCardFooter({ profilepic, data }) {
+function PostCardFooter({ profilepic, data, username }) {
   const [showExplicitly, setShowExplicitly] = useState(true);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [posting_comment, setPostingComment] = useState(false);
+
+  if (data.comments_count > 0 || data.comments_count > "0") {
+    //
+    async function comments_query() {
+      const _collection = collection(db, "comments");
+
+      const _query = query(
+        _collection,
+        where("post_id", "==", data.id),
+        orderBy("date_created", "desc"),
+        limit(2),
+      );
+
+      const Snapshot = await getDocs(_query);
+      const list = Snapshot.docs.map((doc) => doc.data());
+      return list;
+    }
+
+    // const query = db
+    //   .collection("comments")
+    //   .where("post_id", "==", post_id)
+    //   .orderBy("date_created", "desc")
+    //   .limit(2);
+
+    const handle_get_comments = async () => {
+      await comments_query()
+        .then((data) => {
+          setComments(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    useEffect(() => {
+      // console.log("ran");
+
+      handle_get_comments();
+
+      // query.get().then((data) => {
+      //   let __com = [];
+      //   data.forEach((x) => {
+      //     __com.push(x.data());
+      //   });
+      //   setComments(__com);
+      //   hasLoaded();
+      // });
+    }, [data.id]);
+  }
+
+  const handleSubmit = () => {
+    setPostingComment(true);
+    axios
+      .post(`/post/comment/${data.id}`, {
+        post_id: data.id,
+        comment: comment,
+      })
+      .then(() => {
+        setShowExplicitly(true);
+        let c_comments = comments;
+        c_comments.push({
+          comment_text: comment,
+          commenter_username: username,
+        });
+        setComments(c_comments);
+        setPostingComment(false);
+        setComment("");
+      })
+      .catch((err) => {
+        console.log(err);
+        setPostingComment(false);
+      });
+  };
+
+  // console.log({ comment: data.id });
   return (
     <>
       <View
@@ -65,38 +154,63 @@ function PostCardFooter({ profilepic, data }) {
       {showExplicitly && (
         <Animatable.View duration={2000} animation="fadeIn">
           <View style={styles.container}>
-            <View>
-              <View
-                style={{
-                  flexDirection: "row",
-                }}
-              >
-                <Text style={styles.c_username}>~ pabie</Text>
-                <Text
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                  style={styles.c_comment}
+            <View
+              style={
+                comments.length > 0 && {
+                  marginTop: 10,
+                }
+              }
+            >
+              {comments.map((x, index) => (
+                <View
+                  key={x.comment_id ? x.comment_id : "c_" + index}
+                  style={{
+                    flexDirection: "row",
+                    marginBottom: 5,
+                    alignItems: "center",
+                  }}
                 >
-                  Tot tot eoijsoesoe oeisoijes oesieis oeijsoes eosijeos eoisjes
-                  you
-                </Text>
-              </View>
+                  <Text style={styles.c_username}>
+                    - {x.commenter_username}
+                  </Text>
+                  <Text
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                    style={styles.c_comment}
+                  >
+                    {x.comment_text}
+                  </Text>
+                </View>
+              ))}
             </View>
 
             <View
               style={{
-                marginTop: 15,
+                marginTop: 10,
                 flexDirection: "row",
                 alignItems: "center",
+                position: "relative",
               }}
             >
+              {posting_comment && (
+                <Animatable.View
+                  animation="fadeIn"
+                  style={styles.posting_overlay}
+                >
+                  <Text style={styles.posting_overlay_text}>Posting...</Text>
+                </Animatable.View>
+              )}
+
               <Image style={styles.profilepic} uri={profilepic} />
               <TextInput
+                onChangeText={(e) => setComment(e)}
                 placeholderTextColor={colors.secondary_2}
                 placeholder="Write your comment"
                 style={styles.input}
+                value={comment}
               />
               <TouchableOpacity
+                onPress={handleSubmit}
                 style={{
                   marginRight: 10,
                 }}
@@ -112,6 +226,26 @@ function PostCardFooter({ profilepic, data }) {
 }
 
 const styles = StyleSheet.create({
+  posting_overlay_text: {
+    fontSize: 14,
+    color: colors.light,
+    fontStyle: "italic",
+  },
+  posting_overlay: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: colors.dark_opacity_2,
+    borderRadius: 100,
+    zIndex: 1,
+    height: "100%",
+    width: "100%",
+  },
   button_text: {
     color: colors.white,
     fontSize: 16,
@@ -149,7 +283,7 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
     paddingBottom: 5,
-    paddingTop: 10,
+    // paddingTop: 10,
   },
 });
 
--- app/util/fb_admin.js
+++ app/util/fb_admin.js
@@ -1,9 +1,13 @@
 // import React from "react";
-import firebase from "firebase/app";
+import firebase, { initializeApp } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-// import { firebaseConfig } from "./fb_config";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./fb_config";
 // firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // const firestore = firebase.firestore();
 
@@ -11,7 +15,10 @@ import "firebase/firestore";
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 // export const auth = firebase.aut;
-// export const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const fb = firebase;
+// export const googleProvider = new firebase.auth.GoogleAuthProvider()e
 
-export default firestore;
+export default db;
+
+// export const fb = firebase;
+
+// export default firestore;
