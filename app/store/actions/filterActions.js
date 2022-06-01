e0f663743b97657bbb5b042460a1cc52e523d04a
Harmony <harmochiky2@gmail.com>
2022-06-01 16:36:47 +0200

    Block and ubblock user

--- app/store/actions/filterActions.js
+++ app/store/actions/filterActions.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const report_content_id = (id) => (dispatch) => {
   dispatch({
     type: "ADD_ID_TO_REPORTED",
@@ -10,6 +12,10 @@ export const block_profile_by_id = (id) => (dispatch) => {
     type: "ADD_PROFILE_ID_TO_BLOCKED",
     payload: id,
   });
+  axios
+    .get(`/block/${id}`)
+    .then(() => {})
+    .catch((err) => console.log(err));
 };
 
 export const unblock_user = (username) => (dispatch) => {
@@ -17,4 +23,8 @@ export const unblock_user = (username) => (dispatch) => {
     type: "UNBLOCK_PROFILE_USERNAME",
     payload: username,
   });
+  axios
+    .get(`/unblock/${username}`)
+    .then(() => {})
+    .catch((err) => console.log(err));
 };
