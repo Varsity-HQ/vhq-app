7494d7a162b6285b68144cbd7d1517a899c08329
Harmony <harmochiky2@gmail.com>
2022-08-06 03:09:19 +0200

    Progress withg the create ad

--- app/screens/Marketplace/Create.js
+++ app/screens/Marketplace/Create.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import Header from "../../components/headers/header3";
+import Screen from "../../components/Screen";
+import Text from "../../components/AppText";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Entypo } from "@expo/vector-icons";
+import colors from "../../config/colors";
 
 function Create(props) {
-  return <View style={styles.container}></View>;
+  return (
+    <Screen>
+      <Header noBorder backBtnText="Discard" />
+      <View style={[styles.wrapper, styles.first_sec]}>
+        <Entypo name="shop" color={colors.primary} size={40} />
+        <Text style={styles.heading}>Create AD</Text>
+        <Text style={styles.sub_text}>
+          Select a department that relates to your ad
+        </Text>
+      </View>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  wrapper: {
+    padding: 10,
+  },
+  heading: {
+    fontSize: RFValue(30),
+    // fontWeight: "700",/
+    fontFamily: "Lobster-Regular",
+  },
+  first_sec: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    paddingHorizontal: 10,
+  },
+  sub_text: {
+    textAlign: "center",
+  },
 });
 
 export default Create;
